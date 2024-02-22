92b4e48e8ac22db40e73dbe834ecf343282e7236
nthduc <nguyenthaiduc0212@gmail.com>
2024-02-22 16:38:36 +0700

    update

---
+++ jobms/jobms/src/main/java/com/nthduc/jobms/job/impl/JobServiceImpl.java
@@ -0,0 +1,64 @@
+package com.nthduc.jobms.job.impl;
+
+import com.nthduc.jobms.job.JobRepository;
+import com.nthduc.jobms.job.JobService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JobServiceImpl implements JobService {
+    //private List<Job> jobs = new ArrayList<>();
+    private final JobRepository jobRepository;
+
+    public JobServiceImpl(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+    private Long nextId = 1L;
+    @Override
+    public List<Job> findAll() {
+        return jobRepository.findAll();
+    }
+
+    @Override
+    public void createJob(@RequestBody Job job) {
+        job.setId(nextId++);
+        jobRepository.save(job);
+    }
+
+    @Override
+    public Job getJobById(Long id) {
+        return jobRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public boolean deleteJobById(Long id) {
+        try {
+            jobRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean updateJob(Long id, Job updateJob) {
+        Optional<Job> jobOptional  = jobRepository.findById(id);
+        if(jobOptional.isPresent()){
+            Job job = jobOptional.get();
+            job.setTitle(updateJob.getTitle());
+            job.setDescription(updateJob.getDescription());
+            job.setMinSalary(updateJob.getMinSalary());
+            job.setMaxSalary(updateJob.getMaxSalary());
+            job.setLocation(updateJob.getLocation());
+            jobRepository.save(job);
+            return true;
+
+        }
+        return false;
+    }
+}
---
+++ reviewms/reviewms/src/main/java/com/nthduc/reviewms/review/impl/ReviewServiceImpl.java
@@ -0,0 +1,66 @@
+package com.nthduc.reviewms.review.impl;
+
+import com.nthduc.reviewms.review.Review;
+import com.nthduc.reviewms.review.ReviewRepository;
+import com.nthduc.reviewms.review.ReviewService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    @Override
+    public List<Review> getAllReviews(Long companyId) {
+        List<Review> reviews = reviewRepository.findByCompanyId(companyId);
+        return reviews;
+    }
+
+    @Override
+    public boolean addReview(Long companyId, Review review) {
+        if(companyId != null && review != null) {
+            review.setCompanyId(companyId);
+            reviewRepository.save(review);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Review getReview(Long reviewId) {
+        return reviewRepository.findById(reviewId).orElse(null);
+    }
+
+    @Override
+    public boolean updateReview(Long reviewId, Review updateReview) {
+        Review review = reviewRepository.findById(reviewId).orElse(null);
+        if(review != null){
+            review.setTitle(updateReview.getTitle());
+            review.setDescription(updateReview.getDescription());
+            review.setRating(updateReview.getRating());
+            review.setCompanyId(updateReview.getCompanyId());
+            reviewRepository.save(review);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean deleteReview(Long reviewId) {
+        Review review = reviewRepository.findById(reviewId).orElse(null);
+        if(review != null){
+
+            reviewRepository.delete(review);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
